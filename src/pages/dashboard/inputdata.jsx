eae05122fe08991d2a074a4515a950d8d16e6ef9
ariqrafikusumah <ariqrafikusumah@gmail.com>
2022-11-23 13:34:59 +0700

    upload source

---
+++ src/pages/dashboard/inputdata.jsx
@@ -0,0 +1,10 @@
+// import React from "react";
+// import { Input } from "@material-tailwind/react";
+
+// export default function Inputdata() {
+//     return (
+//     <div className="w-72">
+//         <Input label="Username" />
+//     </div>
+//     );
+// }
